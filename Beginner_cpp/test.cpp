904f9188dcce4252439483b4e155fe44547679b3
karry-crey <848936808@qq.com>
2026-07-08 16:42:38 +0800

    update something

--- Beginner_cpp/test.cpp
+++ Beginner_cpp/test.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<iostream>
+#include<assert.h>
 using namespace std;
 int x = 1;
 //define a namespace within a namespace
@@ -46,12 +47,68 @@ namespace shared {
 //function overloading
 //there can be funtions with the same name,
 // but with diffent numbers or types of parameters
-void swap(int* p1, int* p2) {
+//void swap(int* p1, int* p2) {
+//
+//}
+//void swap(double*p1, double* p2) {
+//
+//}
+//
+//void swap(int& p1, int& p2) {
+//	int tmp = p1;
+//	p1 = p2;
+//	p2 = tmp;
+//}
+//int main() {
+//	int a = 10;
+//	int& b = a;
+//	return 0;
+//}
+
+typedef struct AeqLiist {
+	int* a;
+	int size;
+	int capacity;
+}SL;
+void SLInit(SL& sl, int n = 4) {
+	sl.a = (int*)malloc(n * sizeof(int));
+	sl.size = 0;
+	sl.capacity = n;
+}
+void SLPushBack(SL& sl, int x) {
+	//resize
+	sl.a[sl.size] = x;
+	sl.size++;
 
 }
-void swap(double* p1, double* p2) {
 
+int& SLAt(SL& sl,int i) {
+	assert(i < sl.size);
+	return sl.a[i];
 }
+//int main() {
+//	SL s;
+//	SLInit(s);
+//	SLPushBack(s, 1);
+//	SLPushBack(s, 2);
+//	SLPushBack(s, 3);
+//
+//	for (int i = 0; i < s.size; i++) {
+//		SLAt(s, i)++;
+//	}
+//
+//	for (int i = 0; i < s.size; i++) {
+//		cout << SLAt(s, i) << endl;
+//	}
+//	return 0;
+//}
 int main() {
+	//int x = 1;
+	//const int& r = x * 10;
+	double d = 1.1;
+	const int& r5 = d;
+	int i = d;
+	cout << i <<" "<< d << endl;
+
 	return 0;
 }
